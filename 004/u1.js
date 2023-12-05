b22ace50687f6c76334f58ca19d3ba635ef45545
LaimaLP <pileliene.laima@gmail.com>
2023-12-05 23:59:18 +0200

    static

--- 004/u1.js
+++ 004/u1.js
@@ -1,18 +1,37 @@
-console.log("labas")
+console.log("labas");
+
+class TV {
+  static channels = ["LRT", "LNK", "TV3", "Polonia", "TV1", "Animal"];
+  static listChannels() {
+    console.log("Available channels:");
+    for (const channel of this.channels) {
+      console.log(channel);
+    }
+  }
+
+  constructor(brand, owner) {
+    this.brand = brand;
+    this.owner = owner;
+  }
+  changeChannel(number) {
+    console.log("Changing channel..." + this.constructor.channels[number]);
+  }
 
 
 
-ipilti(kiekis){
-this.kiekis = Math.min(this.turis, this.kiekis +kiekis);
-return this;
-}
-ispilti(){
-    const kiekis = this.kiekis;
-    this.kiekis = 0;
-    return kiekis;
   
 }
+const petras = new TV("Samsung", "Petras");
+const maryte = new TV("LG", "Maryte");
+const bebras = new TV("Sony", "Bebras");
 
-stiklinejeYra(){
-    console.log(this.kiekis)
-}
+petras.changeChannel(2);
+maryte.changeChannel(3);
+bebras.changeChannel(5);
+const newChannels = ["MTV", "VH1", "Fox", "BBC", "NK", "BK"];
+
+TV.channels = newChannels;
+petras.changeChannel(2);
+maryte.changeChannel(3);
+bebras.changeChannel(5);
+TV.listChannels();
